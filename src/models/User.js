d9107ea0911ae5beea755be6b21e6b17d10bcdc6
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-24 22:25:35 +0100

    update

--- src/models/User.js
+++ src/models/User.js
@@ -3,27 +3,14 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     id: {type: String, unique: true},
-    email: {
-        type: String, 
-        unique: true,
-        lowercase: true, 
-        trim: true
-    },
-    firstName: {type: String},
+    email: { type: String, unique: true, lowercase: true, trim: true },
+    firstName: { type: String },
     lastName: {type: String},
     password: {type: String},
-    walletAddress: {
-        type: String,
-        unique: true,
-        required: true,
-    },
+    walletAddress: { type: String, unique: true, required: true, },
     phone: {type: String, trim: true},
     address: {type: String},
-    role: {
-        type: String,
-        enum: Object.values(Roles), 
-        default: Roles.SENDER,
-    },
+    role: { type: String, enum: Object.values(Roles), default: Roles.SENDER, },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
