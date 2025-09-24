2c0a5f91eec154c885d15acbac68d3e25f1af45c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-24 20:17:41 +0100

    update

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 3100,
+    port: process.env.PORT || 3000,
     suiNetwork: process.env.SUI_NETWORK || 'testnet',
-    databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost:27017/myapp',
-    jwtSecret: process.env.JWT
+    jwtSecret: process.env.JWT,
+    mongodUri: process.env.MONGODB_URI,
+
 }
