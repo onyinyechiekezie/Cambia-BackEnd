8e9abf96bdf51cb7243160cf86d69bacaa3b8461
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-24 16:49:53 +0100

    update

--- src/models/Sender.js
+++ src/models/Sender.js
@@ -1,7 +1,9 @@
 const User = require("./User");
 
 class Sender extends User {
-
+    constructor(id, firstName, lastName, phone, email, address, role, walletAddress) {
+        super(id, firstName, lastName, phone, email, address, role, walletAddress);
+    }
 }
 
 module.exports = Sender;
