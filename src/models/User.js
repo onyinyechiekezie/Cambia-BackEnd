324269fdddc0f5d4527d60a792e132b37c4af10c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-24 20:43:00 +0100

    update

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,20 @@
 
-const Roles = require('./Roles');
 
-class User {
-    constructor(id, firstName, lastName, phone, email, address, role, walletAddress) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
         this.role = Object.values(Roles).includes(role);
         this.walletAddress = walletAddress;
     }
 }
 
-module.exports = User;
+const userSchema = new mongoose.Schema({
+    id: {type: String, unique: true},
+    email: {type: String, unique: true},
+    firstName: {type: String},
+    lastName: {type: String},
+    password: {type: String},
+    walletAddress: {type: String},
+    phone: {type: String},
+    address: {type: String},
+    role: {type: String},
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
