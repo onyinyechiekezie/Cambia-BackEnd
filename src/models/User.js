67b0fc1c7779b7d258f014a6b8bfb57c0cdd1275
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-24 22:20:25 +0100

    update

--- src/models/User.js
+++ src/models/User.js
@@ -27,3 +27,26 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
+const Joi = require("joi");
+const Roles = require("../models/Roles");
+
+const registerValidator = Joi.object({
+  email: Joi.string().email().required(),
+  firstName: Joi.string().min(2).max(50).required(),
+  lastName: Joi.string().min(2).max(50).required(),
+  password: Joi.string().min(8).required(),
+  walletAddress: Joi.string().pattern(/^0x[a-fA-F0-9]{64}$/).required(),
+  phone: Joi.string().min(10).max(15).optional(),
+  address: Joi.string().optional(),
+  role: Joi.string().valid(...Object.values(Roles)).default(Roles.SENDER),
+});
+
+const loginValidator = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = {
+  registerValidator,
+  loginValidator,
+};
--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,11 +1,7 @@
+const Joi = require("joi");
+const Roles = require("../models/Roles");
 
-  phone: { type: String, trim: true },
-  address: { type: String },
-  role: {
-    type: String,
-    enum: Object.values(Roles), // ✅ uses Role constants
-    default: Roles.SENDER,
-  },
-}, { timestamps: true });
-
-module.exports = mongoose.model("User", userSchema);
+const registerValidator = Joi.object({
+    email: Joi.string().email().required(),
+    firstName: Joi.string().min(2).max(50).required(),
+});
