7a9cdcbe8a25c81b48069657ec6b36419060e74c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-25 15:03:57 +0100

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,7 @@ class AuthController {
     try {
       const result = await this.authService.register(req.body);
       return res.status(201).json(result);
-    } catch {
+    } catch (error){
       console.error("Register error: ", error.message);
       return res.status(400).json({ status: false, message: error });
     }
@@ -19,7 +19,26 @@ class AuthController {
     try {
       const result = await this.authService.login(req.body);
       return res.status(200).json(result);
+    } catch (error) {
+      console.error("Login error: ", error.message);
+      return res.status(400).json({ status: false, message: error.message})
     }
-  }
+  };
 
+  verify = async (req, res)=> {
+    try {
+      const {token} = req.query;
+      if(!token) {
+        return res.status(400).json({status: false, message: "Token is required"})
+      }
+
+      const payload = this.authService.verifyToken(token);
+      return res.status(200).json({ status: true, payload });
+    } catch (error) {
+      console.error("Verify error: ", error.message);
+      return res.status(401).json({ status: false, message: error.message})
+    }
+  };
 }
+
+module.exports = AuthController;
