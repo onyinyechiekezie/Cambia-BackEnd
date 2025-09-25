1b31d28e72df70ef7e7904bedf5b1dcb045531d4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-25 04:32:59 +0100

    update

--- src/services/authServiceImpl.js
+++ src/services/authServiceImpl.js
@@ -1,6 +1,7 @@
 const AuthService = require('./authService');
 const RegisterRequest = require("../dtos/request/userRegisterReq");
-const AuthResponse = require("../dtos/response/AuthResponse")
+const AuthResponse = require("../dtos/response/AuthResponse");
+const User = require('../models/User');
 
 class AuthServiceImpl extends AuthService {
     constructor() {
@@ -8,7 +9,12 @@ class AuthServiceImpl extends AuthService {
     }
 
     async register(registerRequest) {
-        const validated = RegisterRequest.validate(authData)
+        const validated = RegisterRequest.validate(registerRequest)
+
+        const existingUser = await User.findOne({ email: validated.email });
+        if (existing) throw new Error("Enail already exists");
+        
+        
     }
 
 }
