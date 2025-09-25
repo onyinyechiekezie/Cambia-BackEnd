df243bb617f5ba071e116916ee027b340075b9e2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-25 14:17:18 +0100

    update

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const app = require('./src/app');
 const connectDB = require('./src/config/db');
 require('dotenv').config();
 
-
-
 const PORT = process.env.PORT || 5000;
 
-// Connect DB and start server
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
--- tests/gg.js
+++ tests/gg.js
@@ -1,18 +1,4 @@
-const mongoose = require('mongoose');
-const connectDB = require('./config/db');
-const app = require('./app');
-require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
-
-// Connect DB and start server
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.error("DB connection failed:", err));
 
 
   const express = require('express');
