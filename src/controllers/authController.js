bbe329634bff4b1ab714bd7edf7bf8889e876860
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-25 14:45:47 +0100

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,25 @@
-const AuthServiceImpl = require('../')
+const AuthServiceImpl = require('../services/authServicesImpl');
+
+class AuthController {
+  constructor() {
+    this.authService = new AuthServiceImpl();
+  }
+
+  register = async (req, res) => {
+    try {
+      const result = await this.authService.register(req.body);
+      return res.status(201).json(result);
+    } catch {
+      console.error("Register error: ", error.message);
+      return res.status(400).json({ status: false, message: error });
+    }
+  };
+
+  login = async (req, res)=> {
+    try {
+      const result = await this.authService.login(req.body);
+      return res.status(200).json(result);
+    }
+  }
+
+}
