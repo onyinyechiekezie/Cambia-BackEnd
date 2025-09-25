ec67fec07b9ffb32314006fefdf79bf75837b365
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-25 14:22:00 +0100

    update

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const AuthController = require('../controllers/authController');
+
+const router = express.Router();
+const authController = new AuthController();
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.get('/verify', authController.verify);
+
+module.exports = router;
