c631166b4821fca59ff5e895390f950f77f11143
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-26 19:31:54 +0100

    update

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,23 +1,26 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Roles = require('../models/Roles');
+const { jwtSecret } = require('../config/env');
 
-const AuthMiddleware = (requiredRole) => async (req, res, next) => {
-  try {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) throw new Error('Authentication token missing');
+class AuthMiddleware {
+  static authorize = (requiredRole) => async (req, res, next) => {
+    try {
+      const token = req.header('Authorization')?.replace('Bearer ', '');
+      if (!token) throw new Error('Authentication token missing');
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
-    if (!user || user.role !== requiredRole) throw new Error(`Must be a ${requiredRole}`);
+      const decoded = jwt.verify(token, jwtSecret);
+      const user = await User.findById(decoded.id);
+      if (!user || user.role !== requiredRole) throw new Error(`Must be a ${requiredRole}`);
 
-    req.userId = user._id;
-    req.walletAddress = user.walletAddress;
-    req.role = user.role;
-    next();
-  } catch (error) {
-    res.status(401).json({ status: false, message: 'Authentication failed: ' + error.message });
-  }
-};
+      req.userId = user._id;
+      req.walletAddress = user.walletAddress;
+      req.role = user.role;
+      next();
+    } catch (error) {
+      res.status(401).json({ status: false, message: 'Authentication failed: ' + error.message });
+    }
+  };
+}
 
 module.exports = AuthMiddleware;
--- src/models/Order.js
+++ src/models/Order.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Status = require('./OrderStatus');
 
-
 const OrderSchema = new mongoose.Schema({
   senderID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   vendorID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  products: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},  
-  quantity: { type: Number, required: true },
+  products: [{ 
+    productID: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
+    quantity: { type: Number, required: true, min: 1 },
+  }],  
   totalPrice: { type: Number, default: 0 },
   status: { 
     type: String,
@@ -15,6 +16,8 @@ const OrderSchema = new mongoose.Schema({
   },
   trustlessSwapID: { type: String }, 
   proofOfPackaging: { type: String },
+  verifierAddress: { type: String },
+  unlockKey: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Order', OrderSchema);
