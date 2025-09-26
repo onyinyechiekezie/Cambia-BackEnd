0a7322514349f93c392c3e5a063f24b6c9260de9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-26 12:18:31 +0100

    update

--- src/services/senderServiceImpl.js
+++ src/services/senderServiceImpl.js
@@ -1,5 +1,7 @@
-const BlockchainServicMock = require("./blockChainServiceMock");
-
+const BlockchainServiceMock = require("./blockChainServiceMock");
+const SenderService = require("./senderService");
+const Order = require("../models/Order"); // MongoDB Order model
+const { v4: uuidv4 } = require("uuid");
 class SenderServiceImpl extends SenderService{
     constructor() {
         super();
@@ -7,6 +9,64 @@ class SenderServiceImpl extends SenderService{
     }
 
     async placeOrder(orderRequest){
+        const { senderId, receiverName, receiverWallet, amount, currency, products } = orderRequest;
 
+        if (!senderId || !receiverName || !receiverWallet || !amount || !products || products.length === 0) {
+            throw new Error("Missing required fields or products");
+        }
+
+        return await Order.create({
+            orderId: uuidv4(),
+            senderId,
+            receiverName,
+            receiverWallet,
+            amount,
+            currency: currency || "SUI",
+            products,
+            status: "pending",
+            funded: false,
+        });
     }
+
+    async fundOrder(fundRequest) {
+        const { orderID, amount, walletAddress} = fundRequest;
+
+        const order = await Order.findOne({ orderId });
+            if (!order) throw new Error("Order not found");
+            if (order.funded) throw new Error("Order already funded");
+        
+            // call mocked blockchain service
+            const tx = await this.blockchainService.fundOrderTx(orderId, amount, walletAddress);
+        
+            order.funded = true;
+            order.status = "funded";
+            order.txHash = tx.txHash;
+            await order.save();
+        
+            return { order, tx };
+    }
+
+    async trackOrder(trackRequest) {
+        const { orderId } = trackRequest;
+    
+        const order = await Order.findOne({ orderId });
+        if (!order) throw new Error("Order not found");
+    
+        return order;
+      }
+    
+      async confirmReceipt(confirmRequest) {
+        const { orderId } = confirmRequest;
+    
+        const order = await Order.findOne({ orderId });
+        if (!order) throw new Error("Order not found");
+        if (order.status !== "funded") throw new Error("Order not yet funded");
+    
+        order.status = "completed";
+        await order.save();
+    
+        return order;
+      }
+        
 }
+    module.exports = SenderServiceImpl;
